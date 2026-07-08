248bab33f2ca580e3f98f9d50df008d3113670d1
shinmb107 <134572824+shinmb107@users.noreply.github.com>
2026-07-08 12:38:43 +0900

    Add files via upload

---
+++ day3/ex4-6.c
@@ -0,0 +1,18 @@
+//#include <stdio.h>
+//
+//int main() {
+//
+//	int a = 30;
+//	int res;
+//
+//	res = (a > 10) && (a < 20);
+//	printf("(a > 10) && (a < 20) : %d\n", res);
+//
+//	res = (a < 10) || (a > 20);
+//	printf("(a < b) || (a > 20) : %d\n", res);
+//
+//	res = !(a >= 30);
+//	printf("!(a >= 30) : %d\n", res);
+//
+//	return 0;
+//}
---
+++ day3/ex4-8.c
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//
+//int main() {
+//
+//	int a = 20, b  = 3;
+//	double res;
+//
+//	res = ((double)a) / (b);
+//	printf("a = %d, b = %d\n", a, b);
+//	printf("a / b의 결과 : %.1lf\n", res);
+//
+//	a = (int)res;
+//	printf("(int) %.1lf의 결과 : %d\n", res, a);
+//
+//	return 0;
+//}
