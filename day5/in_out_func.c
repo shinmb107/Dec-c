347c4c83d560521b585b6c33b623c2e28bcefcfb
shinmb107 <134572824+shinmb107@users.noreply.github.com>
2026-07-13 09:23:51 +0900

    Add files via upload

---
+++ day5/in_out_func.c
@@ -0,0 +1,19 @@
+//#include <stdio.h>
+//
+//int func(int a, int b) {		// 함수 안에 변수 int형 a, b 생성 
+//
+//	int ret;					// out 할 변수 선언 
+//	ret = a + b;				// out 할 변수 ret에 전달 할 식 입력 
+//
+//	return ret;					// main에 out 
+//}
+//
+//int main() {
+//
+//	int res;
+//	res = func(10, 20);
+//	printf("결과는 %d 입니다.", res);
+//
+//	return 0;
+//}
+
