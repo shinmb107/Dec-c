23bdb9e5ac5cc78086836737b957d8183aa8faf2
shinmb107 <134572824+shinmb107@users.noreply.github.com>
2026-07-16 09:42:03 +0900

    Add files via upload

---
+++ day7/test.c
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main() {
+//
+////	char ch[10];
+////	fgets(ch, sizeof(ch), stdin);
+////	printf("%s\n", ch);
+//
+//
+//	int ch;
+//	ch = fgetc(stdin);
+//	fputc(ch, stdout);
+//	//printf("%s\n", ch);
+//
+//	return 0;
+//}
