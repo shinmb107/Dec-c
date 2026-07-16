433401f5fbd87dad8d0fb732dabd05c78b9688bc
shinmb107 <134572824+shinmb107@users.noreply.github.com>
2026-07-16 09:42:43 +0900

    Add files via upload

---
+++ day8/ex12-11.c
@@ -0,0 +1,26 @@
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main() {
+//
+//	char str1[80], str2[80];
+//	char *resp;
+//	int size1, size2;
+//
+//
+//	printf("2개의 과일 이름 입력 : ");
+//	scanf("%s%s", str1, str2);
+//	if (strlen(str1) > strlen(str2))
+//		resp = str1;
+//	else
+//		resp = str2;
+//	printf("이름이 긴 과일은 : %s\n", resp);
+//
+//	size1 = strlen(str1);
+//	size2 = strlen(str2);
+//	
+//	printf("str1의 크기 : %d\n", size1);
+//	printf("str2의 크기 : %d\n", size2);
+//	
+//	return 0;
+//}
---
+++ day8/ex13-5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void assign(void);
+
+int main() {
+
+	register int i;
+	auto int sum = 0;
+
+	for(i = 0; i <= 10000; i++) {
+		sum += i;
+	}
+
+	printf("%d\n", sum);
+	printf("%d\n", );
+	scanf("%d", &);
+
+	return 0;
+}
