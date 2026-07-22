0fbccd8e39067b39dec242575c9f963081e0176c
shinmb107 <134572824+shinmb107@users.noreply.github.com>
2026-07-22 09:55:59 +0900

    Add files via upload

---
+++ day05/ex7-2.c
@@ -0,0 +1,24 @@
+//#include <stdio.h>
+//
+//int get_num(void);
+//
+//int main() {
+//
+//	int result;
+//
+//	result = get_num();
+//	printf("반환값 : %d\n", result);
+//
+//	return 0;
+//}
+//
+//int get_num(void) {
+//
+//	int num;
+//
+//	printf("양수 입력 : ");
+//	scanf("%d", &num);
+//
+//	return num;
+//}
+
