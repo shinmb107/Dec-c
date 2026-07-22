cd90c3534602c13e913fa682b6e757d489952ee2
shinmb107 <134572824+shinmb107@users.noreply.github.com>
2026-07-22 09:46:32 +0900

    Add files via upload

---
+++ day04/mycode2.c
@@ -0,0 +1,44 @@
+//#include <stdio.h>
+//
+//int main() {
+//
+//	int a, b;
+//	double res;
+//	char ch;
+//
+//	printf("계산식을 입력하세요.");
+//	scanf("%d %c %d",&a, &ch, &b);
+//
+////	printf("첫번째 숫자를 입력하세요.");
+////	scanf_s("%d", &a);
+////
+////	printf("연산자를 입력하세요.");
+////	scanf_s(" %c", &ch);
+////
+////	printf("두번째 숫자를 입력하세요.");
+////	scanf_s("%d", &b);
+//
+//	switch (ch) {
+//		case '+' :
+//			res = (a + b);
+//			printf("%d + %d = %.1lf", a, b, res);
+//			break;
+//
+//		case '-' :
+//			res = (a - b);
+//			printf("%d - %d = %.1lf", a, b, res);
+//			break;
+//
+//		case '*' :
+//			res = (a * b);
+//			printf("%d * %d = %.1lf", a, b, res);
+//			break;
+//
+//		case '/' :
+//			res = (a / b);
+//			printf("%d / %d = %.1lf", a, b, res);
+//			break;
+//	}
+//
+//	return 0;
+//}
---
+++ day04/mycode3.c
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//
+//int main() {
+//
+//	int a = 1;
+//	int i;
+//	int sum;
+//
+//	for(i = 0; i < 100; i++) {
+//		 sum = sum + (a++);
+//	}
+//
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
