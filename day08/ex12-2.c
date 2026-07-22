3519af0273d1954a39360a7d572fa560871e8419
shinmb107 <134572824+shinmb107@users.noreply.github.com>
2026-07-22 10:05:08 +0900

    Add files via upload

---
+++ day08/ex12-2.c
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//
+//int main() {
+//
+//	char *dessert = "apple";
+//
+//	printf("오늘의 후식은 %s 입니다.\n", dessert);
+//	dessert = "banana";
+//	printf("내일의 후식은 %s 입니다.\n", dessert);
+//
+//	return 0;
+//}
---
+++ day08/ex13-2.c
@@ -0,0 +1,19 @@
+//#include <stdio.h>
+//
+//
+//int main() {
+//
+//	int a = 10, b = 20;
+//	
+//	printf("교환전 a, b 값 : %d, %d\n", a, b);
+//	{
+//		int temp;
+//		
+//		temp = a;
+//		a = b;
+//		b = temp; 
+//	}
+//	printf("교환후 a, b 값 : %d, %d\n", a, b);
+//
+//	return 0;
+//}
