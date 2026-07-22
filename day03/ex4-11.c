5fd2428144046026e69232c80b850d34da9e100f
shinmb107 <134572824+shinmb107@users.noreply.github.com>
2026-07-22 09:55:20 +0900

    Add files via upload

---
+++ day03/ex4-11.c
@@ -0,0 +1,14 @@
+//#include <stdio.h>
+//
+//int main() {
+//
+//	int a = 10, b = 20;
+//	int res = 2;
+//
+//	res = (++a, ++b);
+//	
+//	printf("a : %d, b : %d\n", a, b);
+//	printf("res : %d\n", res);
+//	
+//	return 0;
+//}
---
+++ day03/test-sw.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+
+	//int rank = 2, m = 0;
+	int ch;
+	
+	scanf("선택 : %d\n", ch);
+
+	switch (ch) {
+		case 1:
+			printf("오답");
+			break;
+
+		case 2:
+			printf("정답");
+			break;
+
+		case 3:
+			printf("오답");
+			break;
+
+		case 4:
+			printf("오답");
+			break;
+
+		case 5:
+			printf("오답");
+			break;
+
+		default:
+			printf("오답");
+			break;
+	}
+
+	return 0;
+}
